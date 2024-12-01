ffa0bde48611d9e74ceb4049eaaefc31cd20fe2d
Jordi de Kock <jordidekock@gmail.com>
2024-12-01 10:03:34 +0100

    Day 1 solution

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -5,10 +5,49 @@ struct Day01: AdventDay {
     var data: String
 
     func part1() -> Any {
-        return 1
+        var leftList: [Int] = []
+        var rightList: [Int] = []
+        var distances: Int = 0
+        
+        let _ = data
+            .split(separator: "\n")
+            .forEach { element in
+                let nums = element.split(separator: " ")
+                leftList.append(Int(nums[0]) ?? 0)
+                rightList.append(Int(nums[1]) ?? 0)
+            }
+        
+        leftList = leftList.sorted()
+        rightList = rightList.sorted()
+        
+        leftList.enumerated().forEach { index, left in
+            distances += abs(left - rightList[index])
+        }
+        
+        return distances
     }
 
     func part2() -> Any {
-        return 2
+        var leftList: [Int] = []
+        var rightList: [Int] = []
+        var similarities: Int = 0
+        
+        let _ = data
+            .split(separator: "\n")
+            .forEach { element in
+                let nums = element.split(separator: " ")
+                leftList.append(Int(nums[0]) ?? 0)
+                rightList.append(Int(nums[1]) ?? 0)
+            }
+        
+        leftList = leftList.sorted()
+        rightList = rightList.sorted()
+        
+        leftList.enumerated().forEach { index, left in
+            let amount = rightList.count(where: { $0 == left })
+            similarities += left * amount
+        }
+        
+        return similarities
     }
 }
--- Tests/Day01.swift
+++ Tests/Day01.swift
@@ -5,20 +5,30 @@ import XCTest
 final class Day01Tests: XCTestCase {
     // Smoke test data provided in the challenge question
     let testData = """
-        1
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
         """
 
     func testPart1() throws {
         let challenge = Day01(data: testData)
-        XCTAssertEqual(String(describing: challenge.part1()), "1")
+        XCTAssertEqual(String(describing: challenge.part1()), "11")
     }
     
     let testData2 = """
-        2
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
         """
 
     func testPart2() throws {
         let challenge = Day01(data: testData2)
-        XCTAssertEqual(String(describing: challenge.part2()), "2")
+        XCTAssertEqual(String(describing: challenge.part2()), "31")
     }
 }
