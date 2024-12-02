dd1d82662b1ec2cf63fb897b8ee02bbf5aa58b5c
Jordi de Kock <jordidekock@gmail.com>
2024-12-02 10:04:47 +0100

    Day 2 solution

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -5,10 +5,53 @@ struct Day02: AdventDay {
     var data: String
 
     func part1() -> Any {
-        return 1
+        var safeCount = 0
+        let reports: [[Int]] = data.split(separator: "\n").map({ $0.split(separator: " ").map({ Int($0) ?? 0 }) })
+        
+        reports.forEach { report in
+            if checkReport(report) {
+                safeCount += 1
+            }
+        }
+        
+        return safeCount
     }
 
     func part2() -> Any {
-        return 2
+        var safeCount = 0
+        let reports: [[Int]] = data.split(separator: "\n").map({ $0.split(separator: " ").map({ Int($0) ?? 0 }) })
+        
+        reports.forEach { report in
+            if checkReport(report) {
+                safeCount += 1
+            } else {
+                for index in 0..<report.count {
+                    var newreport = report
+                    newreport.remove(at: index)
+                    
+                    if checkReport(newreport) {
+                        safeCount += 1
+                        break
+                    }
+                }
+            }
+        }
+        
+        return safeCount
+    }
+    
+    func checkReport(_ report: [Int]) -> Bool {
+        if report.sorted() == report || report.sorted() == report.reversed() {
+            let result = zip(report, report.dropFirst(1)).map { abs($0 - $1) }.sorted()
+            if result.last ?? 0 > 3 {
+                return false
+            } else if result.first == 0 {
+                return false
+            }
+        } else {
+            return false
+        }
+        
+        return true
     }
 }
--- Tests/Day02.swift
+++ Tests/Day02.swift
@@ -4,20 +4,30 @@ import XCTest
 final class Day02Tests: XCTestCase {
     // Smoke test data provided in the challenge question
     let testData = """
-        1
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
         """
 
     func testPart1() throws {
-        let challenge = Day01(data: testData)
-        XCTAssertEqual(String(describing: challenge.part1()), "1")
+        let challenge = Day02(data: testData)
+        XCTAssertEqual(String(describing: challenge.part1()), "2")
     }
     
     let testData2 = """
-        2
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
         """
 
     func testPart2() throws {
-        let challenge = Day01(data: testData2)
-        XCTAssertEqual(String(describing: challenge.part2()), "2")
+        let challenge = Day02(data: testData2)
+        XCTAssertEqual(String(describing: challenge.part2()), "4")
     }
 }
