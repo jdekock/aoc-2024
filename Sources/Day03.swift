a2462ee1b0d12974c5e02bebd363cc6f0018b21a
Jordi de Kock <jordidekock@gmail.com>
2024-12-03 11:41:08 +0100

    Optimize day 3 solution into 1 regex

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -5,9 +5,8 @@ struct Day03: AdventDay {
     var data: String
 
     func part1() -> Any {
-        let regexmul = try! NSRegularExpression(pattern: "mul\\(\\d{1,},\\d{1,}\\)")
-        
-        let results = regexmul.matches(in: data, range: NSRange(data.startIndex..., in: data))
+        let regex = try! NSRegularExpression(pattern: "mul\\(\\d{1,},\\d{1,}\\)")
+        let results = regex.matches(in: data, range: NSRange(data.startIndex..., in: data))
         
         var total: Int = 0
         results.forEach { result in
@@ -19,34 +18,22 @@ struct Day03: AdventDay {
     }
 
     func part2() -> Any {
-        let regexmul = try! NSRegularExpression(pattern: "mul\\(\\d{1,},\\d{1,}\\)")
-        let regexdo = try! NSRegularExpression(pattern: "do()")
-        let regexdont = try! NSRegularExpression(pattern: "don't()")
-        
-        let results = regexmul.matches(in: data, range: NSRange(data.startIndex..., in: data))
-        let dos = regexdo.matches(in: data, range: NSRange(data.startIndex..., in: data))
-        let donts = regexdont.matches(in: data, range: NSRange(data.startIndex..., in: data))
-        
-        var all: [NSTextCheckingResult] = []
-        all.append(contentsOf: results)
-        all.append(contentsOf: dos)
-        all.append(contentsOf: donts)
-        all = all.sorted(by: { $0.range.location < $1.range.location })
-        
-        var total: Int = 0
+        let results = try! NSRegularExpression(pattern: "do\\(\\)|don't\\(\\)|mul\\(\\d{1,},\\d{1,}\\)").matches(in: data, range: NSRange(data.startIndex..., in: data))
         var skipping = false
         
-        all.forEach { result in
-            if result.regularExpression == regexdont {
+        return results.map({ result in
+            let substring = String(data[Range(result.range, in: data)!])
+            
+            if substring == "don't()" {
                 skipping = true
-            } else if result.regularExpression == regexdo {
+            } else if substring == "do()" {
                 skipping = false
             } else if skipping == false {
-                let nums = String(data[Range(result.range, in: data)!]).replacingOccurrences(of: "mul(", with: "").replacingOccurrences(of: ")", with: "").split(separator:",")
-                total += (Int(nums[0]) ?? 0) * (Int(nums[1]) ?? 0)
+                let nums = substring.replacingOccurrences(of: "mul(", with: "").replacingOccurrences(of: ")", with: "").split(separator:",")
+                return (Int(nums[0]) ?? 0) * (Int(nums[1]) ?? 0)
             }
-        }
-
-        return total
+            
+            return 0
+        }).reduce(0, +)
     }
 }
