4860caf99fbca271c8377667aba9fa3d0a693cae
Jordi de Kock <jordidekock@gmail.com>
2024-12-03 10:21:30 +0100

    Solution for Day 3: Mull It Over

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -5,10 +5,48 @@ struct Day03: AdventDay {
     var data: String
 
     func part1() -> Any {
-        return 1
+        let regexmul = try! NSRegularExpression(pattern: "mul\\(\\d{1,},\\d{1,}\\)")
+        
+        let results = regexmul.matches(in: data, range: NSRange(data.startIndex..., in: data))
+        
+        var total: Int = 0
+        results.forEach { result in
+            let nums = String(data[Range(result.range, in: data)!]).replacingOccurrences(of: "mul(", with: "").replacingOccurrences(of: ")", with: "").split(separator:",")
+            total += (Int(nums[0]) ?? 0) * (Int(nums[1]) ?? 0)
+        }
+        
+        return total
     }
 
     func part2() -> Any {
-        return 2
+        let regexmul = try! NSRegularExpression(pattern: "mul\\(\\d{1,},\\d{1,}\\)")
+        let regexdo = try! NSRegularExpression(pattern: "do()")
+        let regexdont = try! NSRegularExpression(pattern: "don't()")
+        
+        let results = regexmul.matches(in: data, range: NSRange(data.startIndex..., in: data))
+        let dos = regexdo.matches(in: data, range: NSRange(data.startIndex..., in: data))
+        let donts = regexdont.matches(in: data, range: NSRange(data.startIndex..., in: data))
+        
+        var all: [NSTextCheckingResult] = []
+        all.append(contentsOf: results)
+        all.append(contentsOf: dos)
+        all.append(contentsOf: donts)
+        all = all.sorted(by: { $0.range.location < $1.range.location })
+        
+        var total: Int = 0
+        var skipping = false
+        
+        all.forEach { result in
+            if result.regularExpression == regexdont {
+                skipping = true
+            } else if result.regularExpression == regexdo {
+                skipping = false
+            } else if skipping == false {
+                let nums = String(data[Range(result.range, in: data)!]).replacingOccurrences(of: "mul(", with: "").replacingOccurrences(of: ")", with: "").split(separator:",")
+                total += (Int(nums[0]) ?? 0) * (Int(nums[1]) ?? 0)
+            }
+        }
+
+        return total
     }
 }
--- Tests/Day03.swift
+++ Tests/Day03.swift
@@ -5,7 +5,7 @@ import XCTest
 final class Day03Tests: XCTestCase {
     // Smoke test data provided in the challenge question
     let testData = """
-        1
+        xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
         """
 
     func testPart1() throws {
@@ -14,11 +14,11 @@ final class Day03Tests: XCTestCase {
         print("\n🎄 Advent of Code // day 3 // tests")
         print("Test part 1: \(challenge.part1())")
         
-        XCTAssertEqual(String(describing: challenge.part1()), "1")
+        XCTAssertEqual(String(describing: challenge.part1()), "161")
     }
     
     let testData2 = """
-        2
+        xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
         """
 
     func testPart2() throws {
@@ -26,6 +26,6 @@ final class Day03Tests: XCTestCase {
         
         print("Test part 2: \(challenge.part2())\n")
         
-        XCTAssertEqual(String(describing: challenge.part2()), "2")
+        XCTAssertEqual(String(describing: challenge.part2()), "48")
     }
 }
